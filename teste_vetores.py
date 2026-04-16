1d38e64b1c050bee668b54bc2b2b4c278d8ec304
w1esther <w1mariaesther@gmail.com>
2026-04-15 21:12:17 -0300

    verificar vetores ld e li

---
+++ teste_vetores.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+# Seus vetores
+# v1 = [1, 2, 1]
+v2 = [1, 0, 1]
+v3 = [1, 1, 1]
+v4 = [0, 0, 1]
+
+# Criar a matriz
+A = np.array([v4, v3]).T # Transposta para ficarem como colunas
+
+# Calcular o posto o número de vetores linearmente independentes dentro da matriz
+
+posto = np.linalg.matrix_rank(A)
+
+print(f"Posto da matriz: {posto}")
+
+if posto == A.shape[1]:
+    print("Os vetores são Linearmente Independentes.")
+else:
+    print("Os vetores são Linearmente Dependentes (Não formam uma base do R^n).")
