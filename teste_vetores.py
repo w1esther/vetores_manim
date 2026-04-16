09d0e5d3f4ea9349959806a49242c9d2f82ae7c6
w1esther <w1mariaesther@gmail.com>
2026-04-15 21:21:31 -0300

    verificação vetores ld e li

--- teste_vetores.py
+++ teste_vetores.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 # Seus vetores
-# v1 = [1, 2, 1]
+v1 = [1, 2, 1]
 v2 = [1, 0, 1]
 v3 = [1, 1, 1]
-v4 = [0, 0, 1]
+# v4 = [0, 0, 1]
 
 # Criar a matriz
-A = np.array([v4, v3]).T # Transposta para ficarem como colunas
+A = np.array([v1, v3, v2]).T # Transposta para ficarem como colunas
 
 # Calcular o posto o número de vetores linearmente independentes dentro da matriz
 
